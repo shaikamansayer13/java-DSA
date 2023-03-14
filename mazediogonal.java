0b11dfb4fee9d4089ce165cad48a0d3c7c13dfb5
Shaik Aman Sayer <118007035+shaikamansayer13@users.noreply.github.com>
2023-03-14 22:23:17 +0530

    Create mazediogonal.java

---
+++ mazediogonal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(maze("",3,3));
+	}
+	public static ArrayList<String> maze(String p,int r,int c){
+	 if(r==1  && c ==1){
+	     ArrayList<String> list = new ArrayList<>();
+	     list.add(p);
+	     return list;
+	 }
+	     ArrayList<String> ans = new ArrayList<>();
+	     
+	 if(r == c){
+	     ans.addAll(maze(p +'D',r-1,c-1));
+	 }    
+	 if(r>1){
+	     ans.addAll(maze(p + 'H',r-1,c));
+	 }
+	 if(c>1){
+	     ans.addAll(maze(p+'V',r,c-1));
+	 }
+	 return ans;
+	} 
+}
